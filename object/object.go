b234e2ddba6efcb03983068d280c3a2524a79cf0
Oleg Sidorov <oleg@sidorov.nl>
2025-08-02 22:09:34 +0200

    Implement objects: Array, Dictionary, Function

--- object/object.go
+++ object/object.go
@@ -1,6 +1,10 @@
 package object
 
-import "strconv"
+import (
+	"bytes"
+	"osdrv/liss/ast"
+	"strconv"
+)
 
 type ObjectType uint8
 
@@ -12,6 +16,8 @@ const (
 	StringType
 	NullType
 	FunctionType
+	ArrayType
+	DictionaryType
 )
 
 func (t ObjectType) String() string {
@@ -28,6 +34,10 @@ func (t ObjectType) String() string {
 		return "Null"
 	case FunctionType:
 		return "Function"
+	case ArrayType:
+		return "Array"
+	case DictionaryType:
+		return "Dictionary"
 	default:
 		return "Unknown"
 	}
@@ -37,13 +47,46 @@ type Object interface {
 	Type() ObjectType
 	String() string
 	IsNumeric() bool
+	IsBool() bool
+	IsString() bool
 	IsNull() bool
+	IsArray() bool
+	IsDictionary() bool
+	IsFunction() bool
 }
 
-type nonNull struct{}
+type defaultObject struct{}
 
-func (n *nonNull) IsNull() bool {
+func (d defaultObject) IsNumeric() bool {
 	return false
+}
+
+func (d defaultObject) IsBool() bool {
+	return false
+}
+
+func (d defaultObject) IsNull() bool {
+	return false
+}
+
+func (d defaultObject) IsString() bool {
+	return false
+}
+
+func (d defaultObject) IsArray() bool {
+	return false
+}
+
+func (d defaultObject) IsDictionary() bool {
+	return false
+}
+
+func (d defaultObject) IsFunction() bool {
+	return false
+}
+
+type lenable interface {
+	Len() int
 }
 
 type Numeric interface {
@@ -51,21 +94,8 @@ type Numeric interface {
 	Float64() float64
 }
 
-type numeric struct{}
-
-func (n numeric) IsNumeric() bool {
-	return true
-}
-
-type nonNumeric struct{}
-
-func (n *nonNumeric) IsNumeric() bool {
-	return false
-}
-
 type Integer struct {
-	numeric
-	nonNull
+	defaultObject
 	Value int64
 }
 
@@ -84,6 +114,10 @@ func (i *Integer) String() string {
 	return strconv.FormatInt(i.Value, 10)
 }
 
+func (i *Integer) IsNumeric() bool {
+	return true
+}
+
 func (i *Integer) Int64() int64 {
 	return i.Value
 }
@@ -93,8 +127,7 @@ func (i *Integer) Float64() float64 {
 }
 
 type Float struct {
-	numeric
-	nonNull
+	defaultObject
 	Value float64
 }
 
@@ -121,9 +154,12 @@ func (f *Float) Float64() float64 {
 	return f.Value
 }
 
+func (f *Float) IsNumeric() bool {
+	return true
+}
+
 type Bool struct {
-	nonNumeric
-	nonNull
+	defaultObject
 	Value bool
 }
 
@@ -141,13 +177,17 @@ func (b *Bool) String() string {
 	return strconv.FormatBool(b.Value)
 }
 
+func (b *Bool) IsBool() bool {
+	return true
+}
+
 type String struct {
-	nonNumeric
-	nonNull
+	defaultObject
 	Value string
 }
 
 var _ Object = (*String)(nil)
+var _ lenable = (*String)(nil)
 
 func NewString(value string) *String {
 	return &String{Value: value}
@@ -161,8 +201,20 @@ func (s *String) String() string {
 	return s.Value
 }
 
+func (s *String) IsLenable() bool {
+	return true
+}
+
+func (s *String) Len() int {
+	return len(s.Value)
+}
+
+func (s *String) IsString() bool {
+	return true
+}
+
 type Null struct {
-	nonNumeric
+	defaultObject
 }
 
 var _ Object = (*Null)(nil)
@@ -180,5 +232,165 @@ func (n *Null) String() string {
 }
 
 func (n *Null) IsNull() bool {
+	return true
+}
+
+type Array struct {
+	defaultObject
+	items []Object
+}
+
+var _ Object = (*Array)(nil)
+var _ lenable = (*Array)(nil)
+
+func NewArray(items ...Object) *Array {
+	return &Array{items: items}
+}
+
+func (a *Array) Type() ObjectType {
+	return ArrayType
+}
+
+func (a *Array) String() string {
+	var b bytes.Buffer
+	b.WriteByte('[')
+
+	for i, item := range a.items {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		b.WriteString(item.String())
+	}
+
+	b.WriteByte(']')
+
+	return b.String()
+}
+
+func (a *Array) IsLenable() bool {
+	return true
+}
+
+func (a *Array) Len() int {
+	return len(a.items)
+}
+
+func (a *Array) IsArray() bool {
+	return true
+}
+
+func (a *Array) Items() []Object {
+	return a.items
+}
+
+func (a *Array) GetAt(ix int) (Object, bool) {
+	if ix < 0 || ix >= len(a.items) {
+		return nil, false
+	}
+	return a.items[ix], true
+}
+
+func (a *Array) Append(item Object) {
+	a.items = append(a.items, item)
+}
+
+func (a *Array) SetAt(ix int, item Object) bool {
+	if ix < 0 || ix >= len(a.items) {
+		return false
+	}
+	a.items[ix] = item
+	return true
+}
+
+type Dictionary struct {
+	defaultObject
+	items map[string]Object
+}
+
+var _ Object = (*Dictionary)(nil)
+var _ lenable = (*Dictionary)(nil)
+
+func NewDictionary(items map[string]Object) *Dictionary {
+	return &Dictionary{items: items}
+}
+
+func (d *Dictionary) Type() ObjectType {
+	return DictionaryType
+}
+
+func (d *Dictionary) String() string {
+	var b bytes.Buffer
+	b.WriteByte('{')
+
+	first := true
+	for key, value := range d.items {
+		if !first {
+			b.WriteString(", ")
+		}
+		first = false
+		b.WriteString(strconv.Quote(key))
+		b.WriteString(": ")
+		b.WriteString(value.String())
+	}
+
+	b.WriteByte('}')
+
+	return b.String()
+}
+
+func (d *Dictionary) IsLenable() bool {
+	return true
+}
+
+func (d *Dictionary) Len() int {
+	return len(d.items)
+}
+
+func (d *Dictionary) IsDictionary() bool {
+	return true
+}
+
+type Function struct {
+	defaultObject
+	Name    string
+	Args    []string
+	Program *ast.Program
+}
+
+var _ Object = (*Function)(nil)
+
+func NewFunction(name string, args []string, program *ast.Program) *Function {
+	return &Function{
+		Name:    name,
+		Args:    args,
+		Program: program,
+	}
+}
+
+func (f *Function) Type() ObjectType {
+	return FunctionType
+}
+
+func (f *Function) String() string {
+	var b bytes.Buffer
+	b.WriteString("fn ")
+	b.WriteString(f.Name)
+	b.WriteByte('(')
+
+	for i, arg := range f.Args {
+		if i > 0 {
+			b.WriteString(", ")
+		}
+		b.WriteString(arg)
+	}
+
+	b.WriteByte(')')
+	b.WriteByte('\n')
+	b.WriteString(f.Program.String())
+
+	return b.String()
+}
+
+func (f *Function) IsFunction() bool {
 	return true
 }
