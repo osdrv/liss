586994481df5c84cdadb44cf68d655a228edf1c1
Oleg Sidorov <oleg@sidorov.nl>
2026-05-02 20:11:28 +0200

    Fix core includes: accidentally used import kw

--- src/modules/core.h
+++ src/modules/core.h
@@ -1,8 +1,8 @@
 #ifndef liss_modules_core_h
 #define liss_modules_core_h
 
-#import "object.h"
-#import "vm.h"
+#include "object.h"
+#include "vm.h"
 
 void registerCoreNatives(VM* vm, ObjModule* module);
 
